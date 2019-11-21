b4a289ce0f91d93b70e62faaf8052e1257bedc16
Nirmal Bhagwani <nbhagwani@mobiquityinc.com>
2019-11-21 11:25:22 +0530

    project check in

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+declare const AppboyPlugin: any;
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent {
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+    private statusBar: StatusBar
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+      AppboyPlugin.changeUser("nbhagwani");
+      AppboyPlugin.setEmail('nbhagwani@mobiquityinc.com');
+    });
+  }
+}
